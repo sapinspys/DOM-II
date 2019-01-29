eca781baafba3881fe7cfa8673b93156342e97c8
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-01-29 11:14:27 -0500

    Added mousemove event listener to navigation links

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 // MOUSEOVER
 const funBusImg = document.querySelector('.intro img');
-funBusImg.addEventListener('mouseover', function() {
+funBusImg.addEventListener('mouseover', () => {
     if (Math.round(Math.floor(Math.random() * 2) + 1) === 1) {
         funBusImg.setAttribute('src', `img/fun-bus.jpg`);
     } else {
@@ -10,9 +10,10 @@ funBusImg.addEventListener('mouseover', function() {
 });
 
 // KEYDOWN
-window.addEventListener('keydown', function (e) {
-    if (e.keyCode == '13') {
+window.addEventListener('keydown', event => {
+    if (event.key == 'c') {
         const navContainer = document.querySelector('.main-navigation').style.background = getRandomColor();
+        document.body.style.background = getRandomColor();
     }
 });
 function getRandomColor() {
@@ -27,16 +28,22 @@ function getRandomColor() {
 //  CLICK
 const destinationBtns = document.querySelectorAll('.destination .btn');
 const links = Array.from(document.querySelectorAll('.nav-link'));
-destinationBtns.forEach(btn => btn.addEventListener('click', function (e) {
+destinationBtns.forEach(btn => btn.addEventListener('click', () => {
     alert('Please contact us! Use the highlighted link on the navigation bar after clicking OK below.');
     links[3].style.borderBottom = '2px solid purple';
     links[3].style.paddingBottom = '5px';
 }));
 
 // DBLCLICK
-window.addEventListener('dblclick', function () {
+window.addEventListener('dblclick', () => {
     const navContainer = document.querySelector('.main-navigation').style.background = 'white';
+    document.body.style.background = 'white';
 });
+
+// MOUSEMOVE
+links.forEach(link => link.addEventListener('mousemove', () => {
+    link.style.color = getRandomColor();
+}));
 
 // Using preventDefault to prevent page refresh
 links.forEach(link => link.addEventListener('click', function (event) {
