a18cbcb2c88d5314f08ae0be7417bd83b2ab8e72
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-01-28 23:43:57 -0500

    Added 'keydown' event listener to navigation container

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 // Your code goes here
-
+// MOUSEOVER
 const funBusImg = document.querySelector('.intro img');
 funBusImg.addEventListener('mouseover', function() {
-    funBusImg.setAttribute('src', 'img/fun-bus2.jpg');
-})
+    if (Math.round(Math.floor(Math.random() * 2) + 1) === 1) {
+        funBusImg.setAttribute('src', `img/fun-bus.jpg`);
+    } else {
+        funBusImg.setAttribute('src', `img/fun-bus2.jpg`);
+    }
+});
+
+// KEYDOWN
+window.addEventListener('keydown',setRandomColor);
+function setRandomColor(e) {
+    if (e.keyCode == '13') {
+        const navContainer = document.querySelector('.main-navigation').style.background = getRandomColor();
+    }
+};
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+// DRAG and DROP
