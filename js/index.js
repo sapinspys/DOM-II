2573111cdf9975408e412044a149102107a6ce61
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-01-28 21:15:38 -0500

    Added 'mouseover' event listener to fun bus intro image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+const funBusImg = document.querySelector('.intro img');
+funBusImg.addEventListener('mouseover', function() {
+    funBusImg.setAttribute('src', 'img/fun-bus2.jpg');
+})
